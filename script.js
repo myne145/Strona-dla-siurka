c0f251902ad4a45846e8c512c602adfc6231c5e6
myne145 <antoni.kucz@gmail.com>
2023-05-25 11:23:11 +0200

    finished (for real now)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // let gameName;
-const websiteTitle = 'Vekto\'s Game Reviews';
+const tytulstrony = 'Vekto\'s Game Reviews';
 
-const setReviewTextBasedOnSelectedGame = () => {
-    const gameURLName = new URLSearchParams(window.location.search).get('data');
-    let gameName;
-    switch(gameURLName) {
+const Zmiennazwegryitekstu = () => {
+    const nazwagry = new URLSearchParams(window.location.search).get('data');
+    let tytulgry;
+    switch(nazwagry) {
         case 'mc_1.20':
-            gameName = 'Minecraft Update 1.19';
-            document.getElementById('reviewText').innerHTML = '<strong>W obliczu wydania wersji 1.19 Minecrafta fani ponownie zabrali głos. Krytyka The Wild Update nie skupia się na samej zawartości; pod ostrzałem jest komunikacja z deweloperami.</strong>\n' +
+            tytulgry = 'Minecraft Update 1.19';
+            document.getElementById('tekstopinii').innerHTML = '<strong>W obliczu wydania wersji 1.19 Minecrafta fani ponownie zabrali głos. Krytyka The Wild Update nie skupia się na samej zawartości; pod ostrzałem jest komunikacja z deweloperami.</strong>\n' +
                 '            <br>\n' +
                 '            <br>\n' +
                 '            <br>\n' +
@@ -39,11 +39,11 @@ const setReviewTextBasedOnSelectedGame = () => {
                 '            <br>\n' +
                 '            Pojawiały się również wspominki o obiecanej niegdyś archeologii, na którą gracze wciąż czekają. ExpertInBeingAScrub w swojej dość długiej i jak ją sam opisał, brutalnej opinii na temat Minecrafta 1.19, wspomniał również o jednym aspekcie, który do tej pory nie był poruszany – strona z feedbackiem. Mojang bierze opinie głównie ze strony z opiniami i dzięki nim wprowadził wiele nowych funkcji. Dlaczego więc strona jest tak zła? Większość postów jest usuwana bez wyraźnego powodu, a nowe posty są zagłuszane przez bardziej popularne, ale starsze. Myślę, że strona z opiniami wymaga gruntownej przebudowy i powinna być częściej przywoływana. Na przykład podczas MC Live, gdzie więcej osób mogłoby dowiedzieć się o stronie. Wtedy większa część społeczności będzie mogła wyrazić swoje opinie i sugestie na temat Minecrafta. Czyżby zatem The Wild Update zamiast przyczynić się do ulepszania świata Minecrafta, miał poskutkować polepszeniem komunikacji pomiędzy społecznością a deweloperami? Patrząc na liczbę komentarzy przywołujących ten temat wydaje się to bardzo możliwe.\n' +
                 '       ';
-            document.getElementById('gameImage').src = 'mc1.19.jpg';
+            document.getElementById('zdjeciegry').src = 'mc1.19.jpg';
             break;
         case 'tloup1':
-            gameName = 'The Last Of Us Part I';
-            document.getElementById('reviewText').innerHTML = '<strong>The Last of Us Part I</strong> to zdecydowanie jedna z najlepszych, najbardziej wciągających i najlepiej dopracowanych gier w historii naszej branży. Oczywiście, gdy mamy na myśli wersję konsolową. Pierwotnie wydana w 2013 roku na PlayStation 3, doczekała się solidnego remake\'u na PS5, by po kilku miesiącach wylądować na PC. W niniejszym tekście skoncentruję się wyłącznie na aspektach technicznych, pokazując jak działa i co oferuje wersja przeznaczona na "blaszaki". Ocena, którą znajdziecie na samym dole, dotyczy wyłącznie jakości konwersji i nie odnosi się w żaden sposób do rozgrywki, fabuły, klimatu i ogólnej oceny samej gry.\n' +
+            tytulgry = 'The Last Of Us Part I';
+            document.getElementById('tekstopinii').innerHTML = '<strong>The Last of Us Part I</strong> to zdecydowanie jedna z najlepszych, najbardziej wciągających i najlepiej dopracowanych gier w historii naszej branży. Oczywiście, gdy mamy na myśli wersję konsolową. Pierwotnie wydana w 2013 roku na PlayStation 3, doczekała się solidnego remake\'u na PS5, by po kilku miesiącach wylądować na PC. W niniejszym tekście skoncentruję się wyłącznie na aspektach technicznych, pokazując jak działa i co oferuje wersja przeznaczona na "blaszaki". Ocena, którą znajdziecie na samym dole, dotyczy wyłącznie jakości konwersji i nie odnosi się w żaden sposób do rozgrywki, fabuły, klimatu i ogólnej oceny samej gry.\n' +
                 '            <br>\n' +
                 '            <br>\n' +
                 '            <br>\n' +
@@ -56,11 +56,11 @@ const setReviewTextBasedOnSelectedGame = () => {
                 '            <br>\n' +
                 '            Na wspomnianym wyżej komputerze z wykorzystaniem DLSS 2 w trybie jakości w rozdzielczości 4K jest średnio ok. 120 klatek z okazjonalnymi spadkami w okolice 95-100 (po ostatnich aktualizacjach). Przez pierwsze trzy godziny zabawy trafiłem na jedną długą przycinkę, która trwała kilka sekund i bardzo rzadkie mikroprzycięcia. Najnowszy patch o numerze 1.0.1.7 pozwolił jednak na całkowite ich wyeliminowanie, a gra działa jeszcze lepiej. Widać to zresztą po stale rosnącej wartości procentowej ocen na Steam. Poniżej znajdziecie galerię zdjęć, która niestety nie oddaje najlepiej tego, co widać na ekranie monitora. Wersja na PC wspiera dźwięk 3D i obsługuje kontroler DualSense z pełnym zestawem wibracji i adaptacyjnych triggerów (ale tylko przy podłączeniu kablem USB). Ma także szereg dodatkowych ustawień dostępności dla osób niepełnosprawnych i znakomite wsparcie dla klawiatury i myszki. Celowanie na "gryzoniu" stało się teraz niezwykle precyzyjne i komfortowe, a kolejne headshoty sypią się aż miło. Przy tak wysokim poziomie brutalności i jakości samych animacji, mogę powiedzieć, że aż miło się ogląda te kolejne zabójstwa - jakkolwiek dziwnie to nie zabrzmiało.\n' +
                 '       ';
-            document.getElementById('gameImage').src = 'tlou1.jpg';
+            document.getElementById('zdjeciegry').src = 'tlou1.jpg';
             break;
         case 'hogwartslegacy':
-            gameName = 'Hogwarts Legacy';
-            document.getElementById('reviewText').innerHTML = '<strong>\n' +
+            tytulgry = 'Hogwarts Legacy';
+            document.getElementById('tekstopinii').innerHTML = '<strong>\n' +
                 '                Świat czarodziejów J.K. Rowling to jedno z najlepszych IP, które od lat nie otrzymuje odpowiedniej uwagi. Nie możemy przeczytać nowych książek, nie oglądamy filmów, a wielu marzy o serialach, ale tę zdecydowanie za długą ciszę przerwało Avalanche Software, które... nie zawiodło. Nie wierzyłem, że po latach spełnią się moje marzenia o zostaniu uczniem Hogwartu – Wy też chcieliście przemykać po salach i czerpać garściami z wiedzy profesorów? To przeczytajcie naszą recenzję Hogwarts Legacy.\n' +
                 '            </strong>\n' +
                 '            <br>\n' +
@@ -78,97 +78,84 @@ const setReviewTextBasedOnSelectedGame = () => {
                 '            <br>\n' +
                 '            Główny bohater jednak nie jest... zwykłym nastolatkiem, który niespodziewanie otrzymał list z Hogwartu. Już pierwsze 30 minut gry potwierdzają jedno – gracze mają okazję kontrolować wyjątkową personę, w której drzemie pewna moc. To właśnie on bez przeszkód uczy się kolejnych zaklęć, z miejsca chwyta za miotłę i pokazuje swoje umiejętności oblatując budynki szkoły, a nie ma też problemu z ujarzmieniem gryfa. Po kilku pierwszych misjach usłyszycie, jak inne dzieciaki zaczynają gadać o „nowym uczniu”, który z miejsca trafia na języki całej szkoły... brzmi znajomo? Hogwarts Legacy w naprawdę wielu miejscach czerpie garściami z dorobku J.K. Rowling wrzucając do murów zamczyska jednostkę, która niczym Harry Potter staje się tą „najbardziej rozpoznawalną”. Twórcy oczywiście nie rzucili na jego czoło tajemniczej blizny i nie zbudowali mrocznej legendy, ale bardzo szybko okazuje się, że gracze mają okazję uczestniczyć w ciekawej intrydze, która jest budowana przez około 25 godzin. Deweloperzy sięgnęli nawet po ciekawe nawiązanie do pierwszej przygody Pottera, gdy ten musiał udać się na Ulicę Pokątną, by nabyć kilka podstawowych rzeczy na pierwsze dni nauki... w Hogwarts Legacy mamy niemal identyczny wątek.\n' +
                 '       ';
-            document.getElementById('gameImage').src = 'hogwartslegacy.jpg';
+            document.getElementById('zdjeciegry').src = 'hogwartslegacy.jpg';
             break;
     }
 
-    document.getElementById("gameReviewTitle").innerHTML = gameName;
-    if(typeof gameName === "undefined")
+    document.getElementById("tytulopinii").innerHTML = tytulgry;
+    if(typeof tytulgry === "undefined")
         document.title = 'Coś poszło nie tak';
     else
-        document.title = `${gameName} review - ${websiteTitle}`;
-    displayLoggedInAccountOnPageLoad();
+        document.title = `Recenzja ${tytulgry} - ${tytulstrony}`;
+    zmienzalogowanegouzytkownika();
 }
 
-const isInLoginMode = () => {
-    let isLogin;
-    const requestedFormType = new URLSearchParams(window.location.search).get('type');
-    if(requestedFormType === 'login')
-        isLogin = true;
-    else if(requestedFormType === 'register')
-        isLogin = false;
-    else {
-        //TODO: handle invalid values (or maybe not fuck it)
-    }
-    return isLogin;
+const czyjesttryblogowania = () => {
+    let czylogujesie;
+    const typ = new URLSearchParams(window.location.search).get('type');
+    if(typ === 'login')
+        czylogujesie = true;
+    else if(typ === 'register')
+        czylogujesie = false;
+    return czylogujesie;
 }
 
-const setLoginOrRegisterForm = () => {
-    const isLogin = isInLoginMode();
-    if(isLogin === true) {
-        document.getElementById('loginConfirm').innerHTML = "Zaloguj się";
-        document.getElementById('doUWantToRegister').innerHTML = "Nie masz konta? Zarejestruj się!";
-        document.getElementById('loginTitle').innerHTML = "Zaloguj się";
-        document.getElementById('repeatPasswordBox').style.display = 'none';
+const ustawloginiregister = () => {
+    const czylogujesie = czyjesttryblogowania();
+    if(czylogujesie === true) {
+        document.getElementById('potwierdzlogin').innerHTML = "Zaloguj się";
+        document.getElementById('czycheszsiearejestrowac').innerHTML = "Nie masz konta? Zarejestruj się!";
+        document.getElementById('tytulokna').innerHTML = "Zaloguj się";
+        document.getElementById('powtorzhaslobox').style.display = 'none';
     }
-    if(isLogin !== true) {
-        document.getElementById('loginConfirm').innerHTML = "Zarejestruj się";
-        document.getElementById('doUWantToRegister').innerHTML = "";
-        document.getElementById('loginTitle').innerHTML = "Zarejestruj się";
-        document.getElementById('repeatPasswordBox').style.display = 'inline';
+    if(czylogujesie !== true) {
+        document.getElementById('potwierdzlogin').innerHTML = "Zarejestruj się";
+        document.getElementById('czycheszsiearejestrowac').innerHTML = "";
+        document.getElementById('tytulokna').innerHTML = "Zarejestruj się";
+        document.getElementById('powtorzhaslobox').style.display = 'inline';
     }
-    displayLoggedInAccountOnPageLoad();
+    zmienzalogowanegouzytkownika();
 }
 
 
-const updateLoggedInAccount = () => {
-    const isLogin = isInLoginMode();
-    //TODO password checks
-    if(isLogin === true) {
-
-    }
-}
-
-const displayLoggedInAccountOnPageLoad = () => {
+const zmienzalogowanegouzytkownika = () => {
     const username = new URLSearchParams(window.location.search).get('username');
-    const password = new URLSearchParams(window.location.search).get('password');
-    const userInfoDiv = document.getElementById('loggedUserInfo');
-    if (password === null || username === null || password === undefined || username === undefined) {
-        userInfoDiv.style.visibility = 'hidden';
-        document.getElementById('headerLogin').style.left = '40%';
-        document.getElementById('headerRegister').style.left = '40%';
+    const haslo = new URLSearchParams(window.location.search).get('password');
+    const infoouzytkowniku = document.getElementById('infouzalogowanym');
+    if (haslo === null || username === null || haslo === undefined || username === undefined) {
+        infoouzytkowniku.style.visibility = 'hidden';
+        document.getElementById('zalogujsienaglowek').style.left = '60%';
+        document.getElementById('zarejestrujsienaglowek').style.left = '60%';
     } else {
-        userInfoDiv.style.visibility = 'visible';
-        userInfoDiv.innerHTML = `Zalogowano jako ${username}`;
-        document.getElementById('headerLogin').style.visibility = 'hidden';
-        document.getElementById('headerRegister').style.visibility = 'hidden';
-
-        document.getElementById('headerContacts').setAttribute('onclick', `contacts.html?username=${username}&password=${password}`);
-        document.getElementById('game1').setAttribute('href', `reviewpage.html?data=mc_1.201&username=${username}&password=${password}`);
-        document.getElementById('game2').setAttribute('href', `reviewpage.html?data=tloup1&username=${username}&password=${password}`);
-        document.getElementById('game3').setAttribute('href', `reviewpage.html?data=hogwartslegacy&username=${username}&password=${password}`);
+        infoouzytkowniku.style.visibility = 'visible';
+        infoouzytkowniku.innerHTML = `Zalogowano jako ${username}`;
+        document.getElementById('zalogujsienaglowek').style.visibility = 'hidden';
+        document.getElementById('zarejestrujsienaglowek').style.visibility = 'hidden';
+        document.getElementById('gra1').setAttribute('href', `reviewpage.html?data=mc_1.201&username=${username}&password=${haslo}`);
+        document.getElementById('gra2').setAttribute('href', `reviewpage.html?data=tloup1&username=${username}&password=${haslo}`);
+        document.getElementById('gra3').setAttribute('href', `reviewpage.html?data=hogwartslegacy&username=${username}&password=${haslo}`);
     }
 
 }
 
-const displayLoggedInAccountAfterButtonClicked = () => {
+const ustawdaneuzytkowikapozalogownaiu = () => {
     const username = document.getElementById('loginBox').value;
-    const password = document.getElementById('passwordBox').value;
-    const userInfoDiv = document.getElementById('loggedUserInfo');
-    if (password === '' || username === '' || username.length > 10) {
-        userInfoDiv.style.visibility = 'hidden';
-        document.getElementById('headerLogin').style.left = '40%';
-        document.getElementById('headerRegister').style.left = '40%';
-        document.getElementById('headerLogin').style.visibility = 'visible';
-        document.getElementById('headerRegister').style.visibility = 'visible';
+    const haslo = document.getElementById('passwordBox').value;
+    const infouzytkownika = document.getElementById('infouzalogowanym');
+    if (haslo === '' || username === '' || username.length > 10) {
+        infouzytkownika.style.visibility = 'hidden';
+        document.getElementById('zalogujsienaglowek').style.left = '60%';
+        document.getElementById('zarejestrujsienaglowek').style.left = '60%';
+        document.getElementById('zalogujsienaglowek').style.visibility = 'visible';
+        document.getElementById('zarejestrujsienaglowek').style.visibility = 'visible';
         alert("Niepoprawne hasło lub użytkownik. Nazwa użytkownika nie może przekraczać 10 znaków.")
     } else {
-        userInfoDiv.style.visibility = 'visible';
-        userInfoDiv.innerHTML = `Zalogowano jako ${username}`;
-        document.getElementById('headerLogin').style.visibility = 'hidden';
-        document.getElementById('headerRegister').style.visibility = 'hidden';
+        infouzytkownika.style.visibility = 'visible';
+        infouzytkownika.innerHTML = `Zalogowano jako ${username}`;
+        document.getElementById('zalogujsienaglowek').style.visibility = 'hidden';
+        document.getElementById('zarejestrujsienaglowek').style.visibility = 'hidden';
     }
-    window.location.href = `index.html?username=${username}&password=${password}`;
+    window.location.href = `index.html?username=${username}&password=${haslo}`;
 }
 
 
